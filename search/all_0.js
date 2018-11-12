ad2886b95fda475dfc60c17e2cd8175eae382a39
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-12 19:58:45 +0000

    Deploy artbataev/otus_cpp_5 to github.com/artbataev/otus_cpp_5.git:gh-pages

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_20c_2b_2b_20course_20assignment_205_3a_20vector_20graphics_20editor',['Otus C++ course Assignment 5: Vector Graphics Editor',['../md__r_e_a_d_m_e.html',1,'']]]
+];
