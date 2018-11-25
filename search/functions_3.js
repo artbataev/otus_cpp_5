57c7fd75ad3d7d02bd23ce338c194fa9b226446c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 10:35:27 +0000

    Deploy artbataev/otus_cpp_5 to github.com/artbataev/otus_cpp_5.git:gh-pages

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['get_5felements_5frepresentations',['get_elements_representations',['../classeditor_1_1_graphic_document.html#a93565b2ec11edaac153be34af8886d5d',1,'editor::GraphicDocument']]],
+  ['get_5frepresentation',['get_representation',['../classeditor_1_1figures_1_1_canvas_primitive.html#a5f0d2b38f040d21743e47a29c4e79736',1,'editor::figures::CanvasPrimitive::get_representation()'],['../classeditor_1_1figures_1_1_line.html#a2685918c2ddc4d2c5849aff3b36f22b9',1,'editor::figures::Line::get_representation()'],['../classeditor_1_1figures_1_1_circle.html#a0a108485ad5b172af754e7a730df5b94',1,'editor::figures::Circle::get_representation()'],['../classeditor_1_1figures_1_1_rectangle.html#a1829b139ea8deed4aa09fe303b1b1e73',1,'editor::figures::Rectangle::get_representation()'],['../classeditor_1_1figures_1_1_polygon.html#aa0cb367e4632066f1dbb6e6d5b037bb7',1,'editor::figures::Polygon::get_representation()']]],
   ['graphicdocument',['GraphicDocument',['../classeditor_1_1_graphic_document.html#aedde004baadeae466f6281fb2f78cd23',1,'editor::GraphicDocument::GraphicDocument()=default'],['../classeditor_1_1_graphic_document.html#a5c7d16fe4b38920fa507731a503b79dc',1,'editor::GraphicDocument::GraphicDocument(const std::string &amp;path)']]]
 ];
